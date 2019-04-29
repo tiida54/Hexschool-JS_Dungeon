10aa85ed6168740062ff324e7b8b5d1327a039ff
BulbCat.David <tiida541108@gmail.com>
2019-04-30 01:35:16 +0800

    [ADD] COM - Multiplication , MultiplicationList

---
+++ src/components/demo01/Multiplication.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import './Multiplication.scss';
+
+class Multiplication extends Component {
+  constructor(props) {
+    super(props)
+    this.multipleItems = []
+    console.log(props)
+    for (let i = 1; i < 10; i++) {
+      this.multipleItems.push(
+        <div class="Multiplication-main--content">
+          <p>{this.props.number} x {i} = {this.props.number * i}</p>
+        </div>
+      )
+    }
+  }
+  render() {
+    return (
+      <div class="Multiplication">
+        <div class="Multiplication-main">
+          <div class="Multiplication-main--title">
+            <p>{this.props.number}</p>
+          </div>
+          {this.multipleItems}
+        </div>
+      </div>
+    )
+  }
+}
+
+let listItems = [];
+for (let i = 2; i < 10; i++) {
+  listItems.push(<Multiplication number={i} />)
+}
+// console.log(listItems)
+class MultiplicationList extends Component {
+  render() {
+    return <div class="container">
+      <div class="title-block">
+        <div class="title-block__bar">
+          <div class="title-block__bar--line">
+            <div class="line"></div>
+          </div>
+        </div>
+        <div class="title-block__text center">
+          <h1>九九乘法表</h1>
+          <h3>MULTIPLICATION CHART</h3>
+        </div>
+        <div class="title-block__bar">
+          <div class="title-block__bar--line">
+            <div class="line"></div>
+          </div>
+        </div>
+      </div>
+      {listItems}
+    </div>;
+  }
+}
+
+export default MultiplicationList

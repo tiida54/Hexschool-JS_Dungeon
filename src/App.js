0917b27c8dd8ce45da49fea389a6b97aa20c08f5
BulbCat.David <tiida541108@gmail.com>
2019-04-30 01:33:17 +0800

    [ADD] build react-router-dom
    { BrowserRouter, Route, Switch }

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import demo01 from './components/demo01/Multiplication';
+
+const App = () => (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={demo01} />
+        {/* <Route path="/boss01" component={Boss01} /> */}
+
+      </Switch>
+    </BrowserRouter>
+);
 
 export default App;

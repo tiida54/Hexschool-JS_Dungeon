124fd79aa98c2943e245d605257cc6cb15adc12c
BulbCat.David <tiida541108@gmail.com>
2019-05-05 17:10:09 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import demo01 from './demo01/Multiplication';
 import demo02 from './demo02/Clock';
+import demo03 from './demo03/Calculator';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={demo01} />
+      <Route path="/" exact component={demo03} />
       <Route path="/demo01" component={demo01} />
       <Route path="/demo02" component={demo02} />
+      <Route path="/demo03" component={demo03} />
 
     </Switch>
   </BrowserRouter>

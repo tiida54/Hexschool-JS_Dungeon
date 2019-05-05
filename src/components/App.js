5b1d6879bec7b1bc775dd6638ecc188425fea9b6
BulbCat.David <tiida541108@gmail.com>
2019-05-05 18:26:44 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,12 @@ import demo02 from './demo02/Clock';
 import demo03 from './demo03/Calculator';
 
 const App = () => (
-  <BrowserRouter basename={process.env.hexschool_js_dungeon}>
+  <BrowserRouter basename = {process.env.hexschool_js_dungeon}>
     <Switch>
       <Route path="/" exact component={demo03} />
-      <Route path={process.env.hexschool_js_dungeon + '/demo01'} component={demo01} />
-      <Route path="/demo02" component={demo02} />
-      <Route path="/demo03" component={demo03} />
+      <Route path="/demo01" component={demo01} />
+      <Route path="demo02" component={demo02} />
+      <Route path={process.env.Phexschool_js_dungeon + '/demo03'} component={demo03} />
       {/* <Route component={demo03} /> */}
     </Switch>
   </BrowserRouter>

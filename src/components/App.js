3f042de3f7b2467daf78590dcd81d2b5fabee922
BulbCat.David <tiida541108@gmail.com>
2019-05-05 18:14:55 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ import demo03 from './demo03/Calculator';
 const App = () => (
   <BrowserRouter basename = "/hexschool_js_dungeon">
     <Switch>
-      {/* <Route path="/" exact component={demo03} /> */}
-      <Route path="/hexschool_js_dungeon/demo01" component={demo01} />
+      <Route path="/" exact component={demo03} />
+      <Route path="/demo01" component={demo01} />
       <Route path="/demo02" component={demo02} />
       <Route path="/demo03" component={demo03} />
       {/* <Route component={demo03} /> */}

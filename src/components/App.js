d1c1430234e015a8953bf69de1d6b46733256018
BulbCat.David <tiida541108@gmail.com>
2019-05-07 00:17:54 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter , Route, Switch } from 'react-router-dom';
 
 import demo01 from './demo01/Multiplication';
 import demo02 from './demo02/Clock';
 import demo03 from './demo03/Calculator';
 
 const App = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter  basename='/'>
     <Switch>
-      {/* <Route  path='/' exact component={demo03} /> */}
+      <Route path='/' exact component={demo03} />
       <Route path="/demo01" component={demo01} />
       <Route path="/demo02" component={demo02} />
-      <Route component={demo03} />
+      {/* <Route component={demo03} /> */}
     </Switch>
-  </BrowserRouter>
+  </HashRouter >
 );
 
 export default App;

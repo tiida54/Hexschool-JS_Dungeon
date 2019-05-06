647a8f3f6afd77444001f3db95b2d0bd0317f29c
BulbCat.David <tiida541108@gmail.com>
2019-05-07 00:01:00 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ import demo03 from './demo03/Calculator';
 const App = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
-      <Route path={process.env.PUBLIC_URL + '/demo02'} exact component={demo02} />
-      <Route path={process.env.PUBLIC_URL + '/'} component={demo01} />
+      <Route path='/demo02' exact component={demo02} />
+      <Route path='/demo01' exact component={demo01} />
       {/* <Route path={'/' + process.env.PUBLIC_URL} component={demo02} /> */}
-      <Route path= '/demo03' component={demo03} />
+      <Route component={demo03} />
     </Switch>
   </BrowserRouter>
 );

340f1f63e9820999950288a370cf204f19af1035
BulbCat.David <tiida541108@gmail.com>
2019-05-07 00:24:57 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { HashRouter , Route, Switch } from 'react-router-dom';
+import { HashRouter , Route, Link } from 'react-router-dom';
 
 import demo01 from './demo01/Multiplication';
 import demo02 from './demo02/Clock';
 import demo03 from './demo03/Calculator';
 
-const App = () => (
-  <HashRouter  basename='/'>
-    <Switch>
-      <Route path='/' exact component={demo03} />
-      <Route path="/demo01" component={demo01} />
-      <Route path="/demo02" component={demo02} />
-      {/* <Route component={demo03} /> */}
-    </Switch>
-  </HashRouter >
-);
+class App extends React.Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/demo02">demo02</Link></li>
+            <li><Link to="/demo03">demo03</Link></li>
+          </ul>
+
+          <hr />
+  
+          <Route exact path="/" component={demo01} />
+          <Route path="/demo02" component={demo02} />
+          <Route path="/demo03" component={demo03} />
+
+        </div>
+      </HashRouter>
+    );
+  }
+}
 
 export default App;
